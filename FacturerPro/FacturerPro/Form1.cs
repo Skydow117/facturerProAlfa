af568e08e48dbec7cf5dfd95ec235dddd074a871
David Castillo <David Castillo@DESKTOP-PJR4ETE>
2017-01-30 20:37:15 +0100

    init

---
+++ FacturerPro/FacturerPro/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacturerPro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clientsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clientsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.pcgroundDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'pcgroundDataSet.productes' table. You can move, or remove it, as needed.
+            this.productesTableAdapter.Fill(this.pcgroundDataSet.productes);
+            // TODO: This line of code loads data into the 'pcgroundDataSet.clients' table. You can move, or remove it, as needed.
+            this.clientsTableAdapter.Fill(this.pcgroundDataSet.clients);
+
+        }
+    }
+}
